cff904a9a27ff1f7ebe55e3f478f220cb6181ee3
Sujeeth <sujeeth.anil@gmail.com>
2020-04-14 13:33:46 +0530

    added templates folder,updated app.py,fixed broken links,updates stats page to new template, added analytics to nav bar

--- hotel/admin/app.py
+++ hotel/admin/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request
+
 import mysql.connector
-from flask_mysqldb import MySQL
+#from flask_mysqldb import MySQL
 from flask import jsonify
 from flask_cors import CORS,cross_origin
 import requests
@@ -86,6 +87,55 @@ def del_emp():
 	mydb.commit()
 	cur.close()
 	return "SUCCCESS",200
+
+
+@app.route('/wt2/graph')
+
+def graph():
+	mydb = mysql.connector.connect(host="localhost",user="root",passwd="root",database="hotel")
+	cur=mydb.cursor()
+	#out=[['Task','Hours per Day'],['Work',11],['Eat',2],['Commute',2],['Watch TV',2],['Sleep',7]]
+	out=[['PAYMENT_METHODS','NUMBER_OF_PAYMENTS']]
+	cur.execute("SELECT COUNT(GUEST_NUM), PAYMENT_METHODS FROM payment GROUP BY PAYMENT_METHODS")
+	output=cur.fetchall()
+	for row in output:
+		ar=[]
+		ar.append(row[1])
+		ar.append(row[0])
+		out.append(ar)
+	cur.execute("SELECT COUNT(BILL_ID),SUM(AMOUNT), DATE FROM (SELECT * FROM bill ORDER BY DATE ASC ) AS sub GROUP BY sub.DATE")
+
+	output2=cur.fetchall()
+	out2=[["Date","total money"]]
+	for row in output2:
+		ar=[]
+		ar.append(str(row[2]))
+		ar.append(int(row[1]))
+		#ar.append(int(row[0]))
+		out2.append(ar)
+	cur.execute("SELECT COUNT(EMP_ID), gender FROM employee GROUP BY gender")
+	output3=cur.fetchall()
+	out3=[["gender","count"]]
+	for row in output3:
+		ar=[]
+		ar.append(row[1])
+		ar.append(row[0])
+		out3.append(ar)
+
+	cur.execute("SELECT COUNT(GST_ID), CATEGORY FROM reservation GROUP BY CATEGORY")
+	output4=cur.fetchall()
+	out4=[["ROOM CATEGORY", "NO_OF_RESERVATONS"]]
+	for row in output4:
+		ar=[]
+		ar.append(row[1])
+		ar.append(row[0])
+		out4.append(ar)
+
+
+	return render_template('index.html',datum=out,dat=out2, dat3=out3,dat4=out4)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- hotel/admin/stats.php
+++ hotel/admin/stats.php
@@ -1,15 +1,32 @@
+<!DOCTYPE html>
+
 <?php 
 $con=mysqli_connect("localhost","root","root","hotel");
 $a="select SUM(AMOUNT) as tamt,AVG(AMOUNT) as avg,COUNT(AMOUNT) as cnt from bill";
 $b=mysqli_query($con,$a);
 
 ?>
-<!DOCTYPE html>
+
+
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+
+        <title>Current Room Service</title>
+        <link rel="icon" href="web-icon/favicon.ico" type="image/icon type">
+
+        <!-- Bootstrap core CSS -->
+        <link href="https://getbootstrap.com/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link href="https://getbootstrap.com/docs/4.4/dist/css/bootstrap.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+
+
+
   <style>.navbar-default {
   background-color: #0B235A;
   background-image: none;
@@ -50,19 +67,101 @@ tr:nth-child(even) {
   background-color: #dddddd;
 }
 </style>
-</head>
-<body>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">sunrise</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li><a href="roomreservation.php">NEW RESERVATION</a></li>
 
-    </ul>
-  </div>
-</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <link href="dash.css" rel="stylesheet">
+  </head>
+<body>
+      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Sunrise Hotels</a>
+              <ul class="navbar-nav px-3">
+                  <li class="nav-item text-nowrap">
+                      <a class="nav-link" href="#">Sign out</a>
+                  </li>
+              </ul>
+      </nav>
+      <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="dashboard.php">
+                        <span data-feather="home"></span>
+                        Dashboard
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="roomreservation.php">
+                        <span data-feather="plus-square"></span>
+                        Room Reservation
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="custroom.html">
+                        <span data-feather="activity"></span>
+                            On-Going Room Service
+                        </a>
+                    </li>
+                </ul>
+                <hr/>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="employee_add.html">
+                        <span data-feather="user-plus"></span>
+                            Add Employee
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cur_employees.html">
+                        <span data-feather="users"></span>
+                            View Current Employees
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="employee_delete.html">
+                        <span data-feather="user-minus"></span>
+                            Remove Employees
+                        </a>
+                    </li>
+                </ul>
+                <hr/>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="stats.php">
+                        <span data-feather="bar-chart-2"></span>
+                            View Stats
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="http://127.0.0.1:5000/wt2/graph">
+                        <span data-feather="bar-chart-2"></span>
+                            Analytics
+                        </a>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+      <!--PAGE CONETENT HERE!!!!-->
+
+
+
+
 <div class="container">
 
 <h2>earnings</h2>  
@@ -80,7 +179,7 @@ tr:nth-child(even) {
   <?php 
   $row= mysqli_fetch_array($b,MYSQLI_ASSOC);
 
-	echo"<tr><td>".$row['cnt']."</td><td>$".$row['avg']."</td><td>$".$row['tamt']."</td></tr>";
+    echo"<tr><td>".$row['cnt']."</td><td>$".$row['avg']."</td><td>$".$row['tamt']."</td></tr>";
  ?>
   </tbody>
 </table>
@@ -100,7 +199,7 @@ tr:nth-child(even) {
 
   while($row= mysqli_fetch_array($b,MYSQLI_ASSOC))
   {
-	echo"<tr><td>".$row['FNAME']."</td><td>".$row['SALARY']."</td><td>".$row['JOIN_DATE']."</td></tr>";
+    echo"<tr><td>".$row['FNAME']."</td><td>".$row['SALARY']."</td><td>".$row['JOIN_DATE']."</td></tr>";
   }
  ?>
   </tbody>
@@ -121,13 +220,23 @@ tr:nth-child(even) {
 
   while($row= mysqli_fetch_array($b,MYSQLI_ASSOC))
   {
-	echo"<tr><td>".$row['cnt']."</td><td>".$row['CATEGORY']."</td></tr>";
+    echo"<tr><td>".$row['cnt']."</td><td>".$row['CATEGORY']."</td></tr>";
   }
  ?>
   </tbody>
 </table>
 </div>
- 
 
+
+
+
+
+      </main>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="https://getbootstrap.com//docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+        <script src="dashboard.js"></script></body>       
 </body>
 </html>
+
