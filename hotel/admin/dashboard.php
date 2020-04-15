72bdd189c102eddfca4881194189e86951f58c5a
navneetraju66 <navneetraju66@gmail.com>
2020-04-15 16:54:30 +0530

    updated dashboard.php with backend as flask and front end as angular js

--- hotel/admin/dashboard.php
+++ hotel/admin/dashboard.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" ng-app="notesApp">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -14,7 +14,7 @@
     <!-- Custom styles for this template -->
     <link href="dash.css" rel="stylesheet">
   </head>
-  <body>
+  <body ng-controller="MainCtrl as ctrl">
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
   <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="main_page.html">Sunrise Hotels</a>
   <ul class="navbar-nav px-3">
@@ -79,7 +79,7 @@
           </li>
                     <li class="nav-item">
                         <a class="nav-link" href="http://localhost:5000/wt2/graph">
-                        <span data-feather="bar-chart-2"></span>
+                        <span data-feather="bar-chart"></span>
                             Analytics
                         </a>
                     </li>
@@ -109,34 +109,14 @@
                       <thead class="thead-dark">
                         <tr>
                           <th>TYPE OF ROOM/HALL</th>
-                          <th>COST PER NIGHT</th>
+                          <th>PRICE PER DAY</th>
                         </tr>
                       </thead>
-                      <tbody>
-                        <tr>
-                          <td>Superior Room</td>
-                          <td>Rs. 1400</td>
-                        </tr>
-                        <tr>
-                          <td>Deluxe Room</td>
-                          <td>Rs. 1000</td>
-                        </tr>
-                        <tr>
-                          <td>Guest House</td>
-                          <td>Rs. 2500</td>
-                        </tr>
-                        <tr>
-                          <td>Single Room</td>
-                          <td>Rs. 850</td>
-                        </tr>
-                        <tr>
-                          <td>Big Convention Hall</td>
-                          <td>Rs. 7500</td>
-                        </tr>
-                        <tr>
-                          <td>Small Convention Hall</td>
-                          <td>Rs. 3900</td>
-                        </tr>
+                      <tbody id="myTable">
+                      <tr ng-repeat="item in myobj">
+                          <td>{{item.CATEGORY}}</td>
+                          <td>{{item.PRICE_PER_DAY}}</td>
+                      </tr>
                       </tbody>
           </table>
       </div>
@@ -148,5 +128,48 @@
       <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="https://getbootstrap.com//docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
-        <script src="dashboard.js"></script></body>
+        <script src="dashboard.js"></script>
+        <script
+        src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.19/angular.js"></script>
+        <script>
+        const wait=ms=>new Promise(resolve => setTimeout(resolve, ms)); 
+        function getData(){
+             xhr=new XMLHttpRequest();
+            //xhr.responseType='json';
+            var url='http://localhost:5000/wt2/price/view';
+            xhr.open("GET",url);
+            var glo;
+            xhr.onreadystatechange=giveData;
+            xhr.send();
+            //console.log(glo);
+        }
+        function giveData(){
+            if(this.readyState==4 && this.status==200)
+            {
+                myobj=JSON.parse(xhr.response);
+                //console.log('got data');
+                //this.glo=myobj;
+                //console.log(glo);
+                //console.log(myobj);
+                window.myobj=myobj;
+                
+            }
+        }
+        
+        angular.module("notesApp",[])
+        .controller('MainCtrl',["$scope",function($scope){
+
+            getData();
+            wait(1*1000).then(() => $scope.myobj=window.myobj);
+            wait(1*1000).then(()=>$scope.$apply());
+            wait(1*1000).then(()=>console.log($scope.myobj[0].CATEGORY));
+            setInterval(function(){getData();wait(1*1000).then(() => $scope.myobj=window.myobj);wait(1*1000).then(()=>$scope.$apply());wait(1*1000).then(()=>console.log("updated"));}, 5000);
+            //console.log(window.myobj);
+            //console.log(glo);
+        }]);
+    
+    </script>   
+      
+    
+  </body>
 </html>
